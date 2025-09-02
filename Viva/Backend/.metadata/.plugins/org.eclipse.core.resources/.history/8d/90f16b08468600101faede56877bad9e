a80947b4b94112fbfc9ab4eac8689b9ad54d0a51
Harsh Singh <70627271+Harsh-Singh-5525@users.noreply.github.com>
2025-09-02 12:12:54 +0530

    Delete Viva directory

--- Viva/Backend/.metadata/.plugins/org.eclipse.core.resources/.history/8d/90f16b08468600101faede56877bad9e
+++
@@ -1,7 +0,0 @@
-package com.bankofapis.account.repository;
-
-import com.bankofapis.account.model.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Integer> {
-}
--- Viva/Backend/account-service/src/main/java/com/bankofapis/account/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankofapis.account.repository;
-
-import com.bankofapis.account.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface AccountRepository extends JpaRepository<Account, Integer> {
-    List<Account> findByUserId(Integer userId);
-}
